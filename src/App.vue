0ed6717db4b73037ccc5f05c70997da286a88294
mandakha <mandax.narmandakh@gmail.com>
2024-05-19 21:41:42 +0700

    imp nav

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
-<script setup lang="ts">
-import { RouterLink } from 'vue-router';
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <nav>
-    <RouterLink to="/card-match">Go to Card Match</RouterLink>
-  </nav>
   <main>
     <RouterView />
   </main>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,13 @@
+import { RouterLink } from 'vue-router';
 <template>
-    <div>Hello world</div>
+    <div class="container mx-auto">
+        <div class="grid grid-cols-3 gap-4 justify-center py-8">
+            <RouterLink to="/card-match" class="text-center rounded-lg bg-white/10 py-4">
+                Card Match
+            </RouterLink>
+            <RouterLink to="/" class="text-center rounded-lg bg-white/10 py-4">
+                Coming Soon
+            </RouterLink>
+        </div>
+    </div>
 </template>
